5f271843e3bfd40904b75e4da05fe84f38e0a29b
aigulsharip <aigul.sharip@nu.edu.kz>
2024-04-05 10:11:37 +0500

    Implemented two levels of logging, as well for TrainerWorkload microservices

--- src/main/java/com/example/gymcrmspringsecurity/logging/WebMvcConfig.java
+++ src/main/java/com/example/gymcrmspringsecurity/logging/WebMvcConfig.java
@@ -21,7 +21,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/trainees/**")
                 .addPathPatterns("/trainers/**")
                 .addPathPatterns("/training-types/**")
-                .addPathPatterns("/trainings/**");
+                .addPathPatterns("/trainings/**")
+                .addPathPatterns("/trainings-feign/**");
     }
 }
 
