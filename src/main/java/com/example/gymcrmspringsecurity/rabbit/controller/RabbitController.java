a80bbccf6f62f1f93b7b3584781bff3cdae35522
aigulsharip <aigul.sharip@nu.edu.kz>
2024-04-24 03:22:23 +0500

    Initial addition of RabbitMQ config and messaging

---
+++ src/main/java/com/example/gymcrmspringsecurity/rabbit/controller/RabbitController.java
@@ -0,0 +1,31 @@
+package com.example.gymcrmspringsecurity.rabbit.controller;
+
+
+import com.example.gymcrmspringsecurity.rabbit.sender.MessageSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class RabbitController {
+
+    private final MessageSender messageSender;
+
+    @PostMapping(value = "/send")
+    public ResponseEntity<String> sendMessage(@RequestBody String message) {
+        try {
+            messageSender.sendMessage(message);
+            return new ResponseEntity<>("Message send to RabbitMQ: " + message, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Failed to send message: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+
+}
